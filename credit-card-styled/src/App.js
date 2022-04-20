4d47fe0f6e6e12e2459ddda0d7a98d0bcc4b299a
kothadiyasagar <sagarkothadiya98@gmail.com>
2022-04-21 02:21:51 +0530

    pay

---
+++ credit-card-styled/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Credit from './credit';
+
+function App() {
+  return (
+    <>
+    <Credit/>
+    </>
+  );
+}
+
+export default App;
---
+++ credit-card-styled/src/App.styled.js
@@ -0,0 +1,13 @@
+ import styled from "styled-components"
+export  const Card=styled.div`
+ background-color:red;
+ width:400px;
+ margin:5%;
+ color:white;
+ margin-top:5%;
+ border-radius: 25px;
+ height:200px;
+ margin-right:200px
+ `;
+export let Hr=styled.hr`
+background-color:red;`
